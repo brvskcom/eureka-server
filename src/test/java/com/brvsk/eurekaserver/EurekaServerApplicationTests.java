3af63a4b182f6b1336b07823d6c8bfedfb77752a
Kacper <kacpersjuszb@gmail.com>
2023-09-28 10:07:44 +0200

    first commit

---
+++ src/test/java/com/brvsk/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brvsk.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
